187045b013c76ce7ab075343821264bccaf94b7f
danielcaldas <danielcaldas@sapo.com>
2016-03-07 23:23:06 +0000

    Removed experimental perl's smartmatch from script uttop.pl

--- uttop.pl
+++ uttop.pl
@@ -23,9 +23,15 @@ $tree->parse($youtube_page);
 
 my $songs_string = $tree->findvalue('//div[@id="watch-card-tab0"]/table//tr');
 
-my @durations = ( $songs_string =~ /\d+:\d+/g );
+my @durations = ( $songs_string =~ /\d+:\d+/g ); # Optimize extracting directly to hash?
 my @songs = split /\d+:\d+/, $songs_string;
 pop @songs; # remove "View All"
+
+my (%tracks, $i);
+
+foreach my $s (@songs){
+  $tracks{$s} = $durations[$i++];
+}
 
 my @a = $tree->look_down('_tag', 'a');
 my %hash;
@@ -35,11 +41,11 @@ system("mkdir \"$folder\"");
 
 open(my $f, '>', "info.txt");
 print $f "Top Tracks for: $initial_arg\n";
-my ($i,$download_url);
+my ($download_url);
 foreach my $a (@a) {
   $hash{$a->as_text} = $a->attr('href');
-  if($a->as_text ~~ @songs) {
-    print $f $a->as_text." ($durations[$i++])\n";
+  if($tracks{$a->as_text}) {
+    print $f $a->as_text." (".$tracks{$a->as_text}.")\n";
     $download_url = $URL_BASE.$hash{$a->as_text};
     system("youtube-dl --extract-audio --audio-format mp3 -o \"".$a->as_text.".mp3\" --playlist-end 1 $download_url");
     sleep(2);
